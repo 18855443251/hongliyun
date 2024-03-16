9897f9a6298d5457c722f373f0e219c0790dd262
eu2701 <hongliyun@csii.com.cn>
2024-03-16 14:34:50 +0800

    去除注释

--- vueExample/src/router/bom.js
+++ vueExample/src/router/bom.js
@@ -1,11 +1,4 @@
-/*
- * @Author: your name
- * @Date: 2021-07-27 09:40:11
- * @LastEditTime: 2021-08-23 10:41:35
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \vueDemo\src\router\map.js
- */
+
 export default [{
     name: "userAgent",
     path: "/userAgent",
