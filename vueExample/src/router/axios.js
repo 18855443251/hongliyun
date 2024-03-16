e21542e1fc35df0f3641eb1ca6b8d5e0a2c34e6c
eu2701 <hongliyun@csii.com.cn>
2024-03-16 14:34:14 +0800

    去除注释

--- vueExample/src/router/axios.js
+++ vueExample/src/router/axios.js
@@ -1,11 +1,4 @@
-/*
- * @Author: your name
- * @Date: 2021-06-18 09:31:24
- * @LastEditTime: 2021-08-25 10:44:40
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \app空工程\bank\src\router\my.js
- */
+
 const axios1 = () =>
   import(/* webpackChunkName: "my" */ "@/views/axios/axios1.vue"); // 通讯录
 const axios2 = () =>
