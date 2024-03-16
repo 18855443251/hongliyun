183cd1707e077b41b30b6a86b40584c08e19133c
18855443211 <735011103@qq.com>
2024-03-16 23:39:13 +0800

    d

--- vueExample/src/router/bibao.js
+++ vueExample/src/router/bibao.js
@@ -1,11 +1,3 @@
-/*
- * @Author: your name
- * @Date: 2021-06-18 09:31:24
- * @LastEditTime: 2021-08-25 10:44:40
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \app空工程\bank\src\router\my.js
- */
 const bibao1 = () => import( /* webpackChunkName: "my" */ '@/views/bibao/bibao1.vue') // 通讯录
 
 export default [{
